8e8df2d3256face59eb3c68c469b26992f83efe2
Eric Kreis <erickreis793@gmail.com>
2022-03-30 20:37:44 -0300

    installing and using: eslint

--- src/middlewares/error.ts
+++
@@ -1,9 +0,0 @@
-import { ErrorRequestHandler } from 'express';
-
-const errorMiddleware: ErrorRequestHandler = (err, req, res, next) => {
-  if (err.status) {
-    return res.status(err.status).json({ message: err.message })
-  }
-};
-
-export default errorMiddleware;
